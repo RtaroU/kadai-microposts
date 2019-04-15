cc675b9dd320ad85a87fd5c9f946c3e45b5a3ec3
Rtaro.U <comillioncobillio1819@gmail.com>
2019-04-15 00:30:24 +0000

    kadai-micropost-like

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,15 @@ class User < ApplicationRecord
   has_secure_password
   
   has_many :microposts
+  
   has_many :relationships
   has_many :followings, through: :relationships, source: :follow
+  
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
+  
+  has_many :likes
+  has_many :favorite_microposts, through: :likes, source: :micropost 
   
   def follow(other_user)
     unless self == other_user
@@ -29,5 +34,18 @@ class User < ApplicationRecord
   
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
+  end
+  
+  def like(other_micropost)
+    self.likes.find_or_create_by(micropost_id: other_micropost.id)
+  end
+  
+  def unlike(other_micropost)
+    like = self.likes.find_by(micropost_id: other_micropost.id)
+    like.destroy if like
+  end
+  
+  def like?(other_micropost)
+     self.favorite_microposts.include?(other_micropost)
   end
 end
